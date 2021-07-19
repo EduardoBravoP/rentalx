636953a0ec9765c543655a1672f1944c460d2c20
Eduardo Bravo <eduardobravopires@gmail.com>
2021-07-19 11:11:18 -0300

    created authentication with jwt

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,56 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  }
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error("Email or password incorrect!")
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!")
+    }
+
+    const token = sign({}, "1b60e62562282500c8dd61629addc51d", {
+      subject: user.id,
+      expiresIn: "1d"
+    })
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        email: user.email,
+        name: user.name
+      }
+    }
+
+    return tokenReturn
+  }
+}
+
+export { AuthenticateUserUseCase }
