694dfad90df1c371fa83f23a802c6a7057c32f6e
Eduardo Bravo <eduardobravopires@gmail.com>
2021-07-23 10:55:53 -0300

    add a route to cars routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -3,11 +3,20 @@ import { Router } from "express";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 
 const carsRoutes = Router()
 
 let createCarController = new CreateCarController()
+let listAvailableCarController = new ListAvailableCarsController()
 
-carsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle)
+carsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarController.handle
+)
+
+carsRoutes.get("/available", listAvailableCarController.handle)
 
 export { carsRoutes }
