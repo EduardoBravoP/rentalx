465f5b3c049d58b9cec88678f2acfc8e87bf837b
Eduardo Bravo <eduardobravopires@gmail.com>
2021-07-31 13:46:17 -0300

    created findByIds

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -8,13 +8,16 @@ class SpecificationsRepository implements ISpecificationsRepository {
   constructor() {
     this.repository = getRepository(Specification)
   }
-  async create({ name, description }: ISpecificationsRepositoryDTO): Promise<void> {
+
+  async create({ name, description }: ISpecificationsRepositoryDTO): Promise<Specification> {
     const specification = this.repository.create({
       description,
       name
     })
 
     await this.repository.save(specification)
+
+    return specification
   }
 
   async findByName(name: string): Promise<Specification> {
@@ -23,6 +26,12 @@ class SpecificationsRepository implements ISpecificationsRepository {
     })
 
     return specification
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specifications = await this.repository.findByIds(ids)
+
+    return specifications
   }
 }
 
