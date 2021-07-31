56f3fd5c3305bd809e52d181ce9ca940a58c2490
Eduardo Bravo <eduardobravopires@gmail.com>
2021-07-31 13:47:06 -0300

    add more tests on createCarSpecification

--- src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -1,14 +1,20 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory"
+import { SpecificationsRepositoryInMemory } from "@modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory"
 import { AppError } from "@shared/errors/AppError"
 import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase"
 
 let createCarSpecificationUseCase: CreateCarSpecificationUseCase
 let carsRepositoryInMemory: CarsRepositoryInMemory
+let specificationsRepositoryInMemory: SpecificationsRepositoryInMemory
 
 describe("Create Car Specification", () => {
   beforeEach(() => {
     carsRepositoryInMemory = new CarsRepositoryInMemory()
-    createCarSpecificationUseCase = new CreateCarSpecificationUseCase(carsRepositoryInMemory)
+    specificationsRepositoryInMemory = new SpecificationsRepositoryInMemory()
+    createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
+      carsRepositoryInMemory,
+      specificationsRepositoryInMemory
+    )
   })
 
   it("Should be able to add a new specification to the car", async () => {
@@ -21,7 +27,12 @@ describe("Create Car Specification", () => {
       brand: "Brand",
       category_id: "category",
     })
-    const specifications_id = ["54321"]
+    const specification = await specificationsRepositoryInMemory.create({
+      description: "test",
+      name: "test"
+    })
+
+    const specifications_id = [specification.id]
 
     await createCarSpecificationUseCase.execute({ car_id: car.id, specifications_id })
   })
