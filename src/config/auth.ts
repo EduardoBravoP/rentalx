c69fc8adbb1fa7d2911e8822a73f055a3c692c3e
Eduardo Bravo <eduardobravopires@gmail.com>
2021-08-02 12:20:01 -0300

    created auth variables

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "1b60e62562282500c8dd61629addc51d",
+  secret_refresh_token: "205b636078ff0159511deb82624ab00e",
+  expires_in_token: "15m",
+  expires_in_refresh_token: "30d",
+  expires_in_refresh_token_in_days: 30
+}
