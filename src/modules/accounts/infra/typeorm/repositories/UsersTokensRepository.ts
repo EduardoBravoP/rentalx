5dadae3f2cdc6c5c4d394dbcb648cf5bcd0b50f2
Eduardo Bravo <eduardobravopires@gmail.com>
2021-08-04 17:33:30 -0300

    add findByRefreshToken

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -9,6 +9,7 @@ class UsersTokensRepository implements IUsersTokensRepository {
   constructor() {
     this.repository = getRepository(UserTokens)
   }
+
   async create({ user_id, expires_date, refresh_token }: ICreateUserTokenDTO): Promise<UserTokens> {
     const userToken = this.repository.create({
       expires_date,
@@ -34,6 +35,11 @@ class UsersTokensRepository implements IUsersTokensRepository {
     await this.repository.delete(id)
   }
 
+  async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+    const userToken = await this.repository.findOne({ refresh_token })
+
+    return userToken
+  }
 }
 
 export { UsersTokensRepository }
