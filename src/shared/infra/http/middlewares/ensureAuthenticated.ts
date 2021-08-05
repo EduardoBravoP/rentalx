4ecaeb04654722b77b5a1ea60eada412cde873a1
Eduardo Bravo <eduardobravopires@gmail.com>
2021-08-05 18:43:42 -0300

    changed refresh_token to token on ensureAuthenticated

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -15,7 +15,6 @@ export async function ensureAuthenticated(
   next: NextFunction
 ) {
   const authHeader = request.headers.authorization
-  const usersTokensRepository = new UsersTokensRepository()
 
   if (!authHeader) {
     throw new AppError("Token is missing", 401)
@@ -24,14 +23,7 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(" ")
 
   try {
-    const { sub: user_id } = verify(token, auth.secret_refresh_token) as IPayload
-
-    const usersRepository = new UsersRepository()
-    const user = usersTokensRepository.findByUserIdAndRefreshToken(user_id, token)
-
-    if (!user) {
-      throw new AppError("User does not exists", 401)
-    }
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload
 
     request.user = {
       id: user_id
